87d1cabffb664cbf850a5de601d7c434988f990d
Chakresh2001 <120241122+Chakresh2001@users.noreply.github.com>
2022-12-24 21:27:33 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,1 +1,5 @@
-please write a code it is a request
+let a ="hary";
+let b ="luffy";
+let c ="luffy";
+
+(a==b==c) ? console.log("all names are same ") : (a==b) ? console.log("ab") : (a==c) ? console.log("ac") : (b==c) ? console.log("bc") : console.log("names are different")
